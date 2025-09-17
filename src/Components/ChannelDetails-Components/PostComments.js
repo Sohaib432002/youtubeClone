0b23e0c7438018afb7be7309715a52802974c202
Muhammad Sohaib Maqsood <sohaibmaqsoodsohaibmaqsood@gmail.com>
2025-09-17 08:16:05 +0500

    update the post page

---
+++ src/Components/ChannelDetails-Components/PostComments.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router'
+import Comments from '../PlayerComponent/Comments'
+import CommentSkel from '../PlayerComponent/subComponents/CommentSkel'
+const listbtnIcons = ['flag']
+const listbtnNames = ['Report']
+
+const PostComments = () => {
+  const [options, setOptions] = useState(false)
+  const [commentData, setcommentData] = useState([])
+  console.log(commentData)
+  const [fetchData, setfetchData] = useState([])
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center my-7">
+        <div className="m-1 flex items-start  max-w-[800px] justify-between cursor-pointer border bg-gray-600 rounded-lg">
+          <div className="flex ">
+            <div className="flex justify-center p-2 items-start">
+              <Link to="/profile">
+                <img
+                  src="https://wallpapers.com/images/hd/cool-profile-picture-paper-bag-head-4co57dtwk64fb7lv.jpg"
+                  className="rounded-full m-2"
+                  alt="profile"
+                  width={70}
+                />
+              </Link>
+            </div>
+
+            <div className="flex p-3  flex-col  justify-start font-sans text-white text-[16px]">
+              <Link to="/user/junaid">
+                <span className="cursor-pointer">Junaid Akram • 10 months ago</span>
+              </Link>
+
+              <p className="my-1">
+                We recently hosted an impactful event on countering human smuggling and
+                trafficking...
+              </p>
+
+              <img
+                src="https://media.istockphoto.com/id/1285124274/photo/middle-age-man-portrait.jpg?s=612x612&w=0&k=20&c=D14m64UChVZyRhAr6MJW3guo7MKQbKvgNVdKmsgQ_1g="
+                className="rounded-lg"
+                width={1000}
+                alt="post"
+              />
+
+              <div className="flex justify-start items-center mt-2">
+                <span className="flex items-center">
+                  <span className="hover:bg-gray-500 cursor-pointer mx-1 py-1 rounded-full">
+                    <i className="fa-regular fa-thumbs-up mx-1"></i>
+                  </span>
+                  <span>123</span>
+                  <span className="hover:bg-gray-500 cursor-pointer mx-1 py-1 rounded-full">
+                    <i className="fa-regular fa-thumbs-down mx-1"></i>
+                  </span>
+                </span>
+
+                <span className="flex ml-4">
+                  <span className="hover:bg-gray-500 cursor-pointer mx-1 py-1 rounded-full">
+                    <i className="fa-solid fa-share mx-1"></i>
+                  </span>
+                </span>
+              </div>
+            </div>
+            <div className="flex justify-center items-start ">
+              <span
+                onClick={(e) => {
+                  e.stopPropagation()
+                  setOptions(!options)
+                }}
+                className="hover:bg-gray-500 relative cursor-pointer m-2 p-2 rounded-full"
+              >
+                <i className="fa-solid fa-ellipsis-vertical mx-1"></i>
+
+                {options && (
+                  <div className="z-30 right-[20px] top-[20px] text-[16px] overflow-hidden w-[210px] bg-[#272727] rounded-lg absolute">
+                    {listbtnNames.map((item, i) => {
+                      return (
+                        <div key={i} className="flex flex-col">
+                          <div
+                            onClick={(e) => {
+                              e.stopPropagation()
+                              alert(`${item}, we are working on it`)
+                            }}
+                            className="px-4 hover:bg-[#414140] flex items-center py-2"
+                          >
+                            <i className={`fa-solid mx-2 fa-${listbtnIcons[i]}`}></i>
+                            <p>{item}</p>
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )}
+              </span>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default PostComments
--- src/Components/PlayerComponent/subComponents/CommentSkel.js
+++ src/Components/PlayerComponent/subComponents/CommentSkel.js
@@ -1,53 +1,41 @@
-import React from "react";
+import React from 'react'
 
 const CommentSkel = ({ commentData }) => {
-  console.log("CommentData--------------", commentData);
+  console.log('CommentData--------------', commentData)
+  console.log(!Array.isArray(commentData))
+  const skeletonCount = 4
+
   return (
     <>
-      {commentData.length === undefined ? (
-        <>
-          <div>
-            <h1>Comments are Disabled</h1>
-          </div>
-        </>
+      {!Array.isArray(commentData) ? (
+        <div>
+          <h1 className="text-white">Comments are Disabled</h1>
+        </div>
       ) : (
-        <div className="max-w-[1227px] player text-[20px] text-[#FFFFFF]    aspect-video  overflow-hidden rounded-[10px] ml-20 ">
-          <div className="flex items-center">
-            <p className="w-[10%] rounded-full flex-inline m-3 bg-gray-700 h-[7vh]"></p>
-            <div className="flex flex-col w-[100%]">
-              {" "}
-              <div className="w-[30%] bg-gray-700 h-[2vh] rounded-full"></div>
-              <div className="w-[5%] bg-gray-700 my-1 h-[2vh] rounded-full"></div>
+        <div className="max-w-[1227px] player text-[20px] text-[#FFFFFF] aspect-video overflow-hidden rounded-[10px] ml-20">
+          {console.log('han')}
+          {Array.from({ length: skeletonCount }).map((_, index) => (
+            <div key={index} className="flex items-center mb-3">
+              <p className="w-[10%] rounded-full flex-inline m-3 bg-gray-700 h-[7vh]"></p>
+
+              <div className="flex flex-col w-[100%]">
+                <div
+                  className={`bg-gray-700 h-[2vh] rounded-full ${
+                    index % 2 === 0 ? 'w-[30%]' : 'w-[50%]'
+                  }`}
+                ></div>
+                <div
+                  className={`bg-gray-700 my-1 h-[2vh] rounded-full ${
+                    index % 2 === 0 ? 'w-[5%]' : 'w-[10%]'
+                  }`}
+                ></div>
+              </div>
             </div>
-          </div>
-          <div className="flex items-center">
-            <p className="w-[10%] rounded-full flex-inline m-3 bg-gray-700 h-[7vh]"></p>
-            <div className="flex flex-col w-[100%]">
-              {" "}
-              <div className="w-[20%] bg-gray-700 h-[2vh] rounded-full"></div>
-              <div className="w-[10%] bg-gray-700 my-1 h-[2vh] rounded-full"></div>
-            </div>
-          </div>
-          <div className="flex items-center">
-            <p className="w-[10%] rounded-full flex-inline m-3 bg-gray-700 h-[7vh]"></p>
-            <div className="flex flex-col w-[100%]">
-              {" "}
-              <div className="w-[50%] bg-gray-700 h-[2vh] rounded-full"></div>
-              <div className="w-[10%] bg-gray-700 my-1 h-[2vh] rounded-full"></div>
-            </div>
-          </div>
-          <div className="flex items-center">
-            <p className="w-[10%] rounded-full flex-inline m-3 bg-gray-700 h-[7vh]"></p>
-            <div className="flex flex-col w-[100%]">
-              {" "}
-              <div className="w-[20%] bg-gray-700 h-[2vh] rounded-full"></div>
-              <div className="w-[100%] bg-gray-700 my-1 h-[2vh] rounded-full"></div>
-            </div>
-          </div>
+          ))}
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default CommentSkel;
+export default CommentSkel
